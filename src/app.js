723513b8b44d6ec052696921c206f505dea2a811
Larry Gonzalez <larry.g.m1024@gmail.com>
2020-07-03 11:21:33 -0300

    port

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const ejslint = require('ejs-lint');
 
 // settings
-app.set('port', 5000);
+app.set('port', process.env.PORT ||5000);
 app.set('views',path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
 async function main(){
-    await app.listen(app.get(process.env.PORT ||'port'));
+    await app.listen(app.get('port'));
     console.log('Server on port', app.get('port'));
 };
 main();
